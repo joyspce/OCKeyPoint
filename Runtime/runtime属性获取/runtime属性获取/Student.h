7dc255fe7c40352d9121219caf819da8c9a7cb59
jiwuchao <wuchaoji@pptv.com>
2018-08-08 18:35:38 +0800

    文件

---
+++ Runtime/runtime属性获取/runtime属性获取/Student.h
@@ -0,0 +1,23 @@
+//
+//  Student.h
+//  runtime属性获取
+//
+//  Created by JiWuChao on 2018/8/8.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@property (nonatomic, assign) NSInteger age;
+
+@property (nonatomic, assign) NSInteger heigh;
+
+@property (nonatomic, copy) NSString *address;
+
+@property (nonatomic, copy) NSString *love;
+
+@end
---
+++ Runtime/runtime属性获取/runtime属性获取/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  runtime属性获取
+//
+//  Created by JiWuChao on 2018/8/8.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
