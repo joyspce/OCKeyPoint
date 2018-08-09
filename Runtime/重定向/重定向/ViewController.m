590084bef315bdc70ed99dd5f4ea77d63cacc289
姬武超 <1500536442@qq.com>
2018-08-09 22:45:47 +0800

    方法交换

--- Runtime/重定向/重定向/ViewController.m
+++ Runtime/重定向/重定向/ViewController.m
@@ -24,32 +24,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    Method oriMethod =   class_getInstanceMethod([Person class], @selector(whoImI));
+    Method studMethod =   class_getInstanceMethod([Student class], @selector(myIdentity));
+    method_exchangeImplementations(oriMethod, studMethod);
     
-    Class people = object_getClass([Person class]);
-    Class student = object_getClass([Student class]);
-    ////源方法的SEL和Method
-    SEL peoSel = @selector(whoImI);
-    Method oriMethod =   class_getInstanceMethod(people, peoSel);
-    ////交换方法的SEL和Method
-
-    SEL studSel = @selector(myIdentity);
-    Method studMethod =   class_getInstanceMethod(people, studSel);
-    ////先尝试給源方法添加实现，这里是为了避免源方法没有实现的情况
-    BOOL addSuccess = class_addMethod(people, peoSel, method_getImplementation(studMethod), method_getTypeEncoding(studMethod));
-    
-    if (addSuccess) {
-        // 添加成功：将源方法的实现替换到交换方法的实现
-
-        class_replaceMethod(student, studSel, method_getImplementation(oriMethod), method_getTypeEncoding(oriMethod));
-    } else {
-        //添加失败：说明源方法已经有实现，直接将两个方法的实现交换即
-
-        method_exchangeImplementations(oriMethod, studMethod);
-    }
-    
-    Person *p = [[Person alloc] init];
-    
-    [p whoImI];
+    Person *person = [[Person alloc] init];
+    [person whoImI]; //方法已经被 student 的方法替换
     
 }
 
