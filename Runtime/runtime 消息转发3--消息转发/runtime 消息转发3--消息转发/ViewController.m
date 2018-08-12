6dbb504d67368cccaf41996e7bbf318be541b3fc
姬武超 <1500536442@qq.com>
2018-08-12 17:32:12 +0800

    文件调整

---
+++ Runtime/runtime 消息转发3--消息转发/runtime 消息转发3--消息转发/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  runtime 消息转发3--消息转发
+//
+//  Created by JiWuChao on 2018/8/12.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
