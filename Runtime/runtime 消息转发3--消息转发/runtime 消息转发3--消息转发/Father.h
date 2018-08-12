1b416228060c7c1c7618d5b1b1254bb563b07dab
姬武超 <1500536442@qq.com>
2018-08-12 18:04:45 +0800

    添加文件

---
+++ Runtime/runtime 消息转发3--消息转发/runtime 消息转发3--消息转发/Father.h
@@ -0,0 +1,19 @@
+//
+//  Father.h
+//  runtime消息转发1-动态方法解析
+//
+//  Created by JiWuChao on 2018/8/12.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Grandfather.h"
+
+@interface Father : Grandfather
+
+- (void)gotoWork;
+
+- (void)goHomeToCook;
+
+@end
