25d2c1d78de3f315f12d3eab968765c6623b6173
jiwuchao <wuchaoji@pptv.com>
2018-08-22 11:45:18 +0800

    category

--- Category&&Extension/Category&&Extension/Father+Category.h
+++ Category&&Extension/Category&&Extension/Father+Category.h
@@ -12,6 +12,100 @@
 
 @property (nonatomic,copy) NSString *idNumber;
 
+
+/*
+ category动态扩展了原来类的方法，在调用者看来好像原来类本来就有这些方法似的，有两个事实：
+ 
+ 不论有没有import category 的.h，都可以成功调用category的方法，都影响不到category的加载流程，import只是帮助了编译检查和链接过程
+ runtime加载完成后，category的原始信息在类结构里将不会存在
+ 这需要探究下runtime对category的加载过程，这里就简单说一下
+ 
+ objc runtime的加载入口是一个叫_objc_init的方法，在library加载前由libSystem dyld调用，进行初始化操作
+ 调用map_images方法将文件中的imagemap到内存
+ 调用_read_images方法初始化map后的image，这里面干了很多的事情，像load所有的类、协议和category，著名的+ load方法就是这一步调用的
+ 仔细看category的初始化，循环调用了_getObjc2CategoryList方法，这个方法拿出来看看：
+ …
+ #define GETSECT(name, type, sectname)                                   \
+ type *name(const header_info *hi, size_t *outCount)  \
+ {                                                                   \
+ unsigned long byteCount = 0;                                    \
+ type *data = (type *)                                           \
+ getsectiondata(hi->mhdr, SEG_DATA, sectname, &byteCount);   \
+ *outCount = byteCount / sizeof(type);                           \
+ return data;                                                    \
+ }
+ 
+ // ... //
+ 
+ GETSECT(_getObjc2CategoryList, category_t *, "__objc_catlist");
+ 眼熟的__objc_catlist，就是上面category存放的数据段了，可以串连起来了
+ 
+ 在调用完_getObjc2CategoryList后，runtime终于开始了category的处理，简化的代码如下
+ 
+ // Process this category.
+ // First, register the category with its target class.
+ // Then, rebuild the class's method lists (etc) if
+ // the class is realized.
+ BOOL classExists = NO;
+ if (cat->instanceMethods ||  cat->protocols  ||  cat->instanceProperties)
+ {
+     addUnattachedCategoryForClass(cat, cls, hi);
+     if (isRealized(cls)) {
+             remethodizeClass(cls);
+             classExists = YES;
+         }
+     }
+ 
+     if (cat->classMethods  ||  cat->protocols )
+     {
+         addUnattachedCategoryForClass(cat, cls->isa, hi);
+         if (isRealized(cls->isa)) {
+         remethodizeClass(cls->isa);
+     }
+ }
+ 首先分成两拨，一拨是实例对象相关的调用addUnattachedCategoryForClass，一拨是类对象相关的调用addUnattachedCategoryForClass，然后会调到attachCategoryMethods方法，这个方法把一个类所有的category_list的所有方法取出来组成一个method_list_t **，注意，这里是倒序添加的，也就是说，新生成的category的方法会先于旧的category的方法插入
+ 
+ static void
+ attachCategoryMethods(class_t *cls, category_list *cats,
+ BOOL *inoutVtablesAffected) {
+     if (!cats) return;
+     if (PrintReplacedMethods) printReplacements(cls, cats);
+ 
+     BOOL isMeta = isMetaClass(cls);
+     method_list_t **mlists = (method_list_t **)
+     _malloc_internal(cats->count * sizeof(*mlists));
+ 
+     // Count backwards through cats to get newest categories first
+     int mcount = 0;
+     int i = cats->count;
+     BOOL fromBundle = NO;
+     while (i--) {
+     method_list_t *mlist = cat_method_list(cats->list[i].cat, isMeta);
+     if (mlist) {
+         mlists[mcount++] = mlist;
+         fromBundle |= cats->list[i].fromBundle;
+     }
+ }
+ 
+ attachMethodLists(cls, mlists, mcount, NO, fromBundle, inoutVtablesAffected);
+ 
+ _free_internal(mlists);
+ 
+ }
+ 
+ 生成了所有method的list之后，调用attachMethodLists将所有方法前序添加进类的方法的数组中，也就是说，如果原来类的方法是a,b,c，类别的方法是1,2,3，那么插入之后的方法将会是1,2,3,a,b,c，也就是说，原来类的方法被category的方法覆盖了，但被覆盖的方法确实还在那里。
+ 
+ 以上 ：
+    总体的顺序是 ：倒叙取出category中的方法列表，生成一个中间数组 ，然后中间数组 正序 添加到 原来类的方法列表中
+ 
+ 如果类别的方法是 [m1,m2,m3] 生成的中间数组 是[m3,m2,m1] 如果原来类的生成方法列表是 [ma,mb,m3]
+ 合成之后 就是 [m1,m2,m3,ma,mb,m3] 在方法查找的时候是从前往后查找 首先查到的是 category中的m3 此时就会结束 不会继续向下查找，也就是原来类的方法被 category中的同名方法覆盖了
+ 
+ 
+ 
+ 
+ */
+
 - (void)goShoping;
 
 - (void)goHome;
--- Category&&Extension/Category&&Extension/ViewController.m
+++ Category&&Extension/Category&&Extension/ViewController.m
@@ -16,7 +16,8 @@
 
 #import <objc/runtime.h>
 
-//参考 ：https://tech.meituan.com/DiveIntoCategory.html
+//参考 ：1 https://tech.meituan.com/DiveIntoCategory.html
+// 2 https://blog.sunnyxx.com/2014/03/05/objc_category_secret/
 @interface ViewController ()
 
 @property (nonatomic,strong) Father *father;
@@ -37,12 +38,12 @@
     ** Category的结构体 ：
  
      typedef struct category_t {
-        const char *name;  //类的名字
-        classref_t cls;  //类
-        struct method_list_t *instanceMethods;  //category中所有给类添加的实例方法的列表
-        struct method_list_t *classMethods;  //category中所有添加的类方法的列表
-        struct protocol_list_t *protocols;  //category实现的所有协议的列表
-        struct property_list_t *instanceProperties;  //category中添加的所有属性
+        const char *name;  //注意，并不是category小括号里写的名字，而是类的名字
+        classref_t cls;  //要扩展的类对象，编译期间这个值是不会有的，在app被runtime加载时才会根据name对应到类对象
+        struct method_list_t *instanceMethods;  //这个category所有的-方法
+        struct method_list_t *classMethods;  //category这个category所有的+方法
+        struct protocol_list_t *protocols;  //这个category实现的protocol，比较不常用在category里面实现协议，但是确实支持的
+        struct property_list_t *instanceProperties;  //这个category所有的property，这也是category里面可以定义属性的原因，不过这个property不会@synthesize实例变量，一般有需求添加实例变量属性时会采用objc_setAssociatedObject和objc_getAssociatedObject方法绑定方法绑定，不过这种方法生成的与一个普通的实例变量完全是两码事
      } category_t;
  
    ** objc_method_list 的结构体
@@ -67,6 +68,47 @@
  
     3.1 category 添加属性 不会生成_变量（带下划线的成员变量），也不会生成添加属性的getter和setter方法
     3.1  使用runtime的 关联机制可以动态的为Category中添加的属性 手动setter和getter方法 并在其中做值得关联逻辑
+    3.3 category添加的属性 关联对象又是存在什么地方呢？ 如何存储？ 对象销毁时候如何处理关联对象呢？
+ 
+        3.3.1 关联对象都由AssociationsManager管理，而AssociationsManager定义如下：
+ 
+         class AssociationsManager {
+                static OSSpinLock _lock;
+                static AssociationsHashMap *_map;               // associative references:  object pointer -> PtrPtrHashMap.
+                public:
+                AssociationsManager()   { OSSpinLockLock(&_lock); }
+                ~AssociationsManager()  { OSSpinLockUnlock(&_lock); }
+ 
+                AssociationsHashMap &associations() {
+             if (_map == NULL)
+                _map = new AssociationsHashMap();
+                return *_map;
+             }
+         };
+        3.3.2 AssociationsManager里面是由一个静态AssociationsHashMap来存储所有的关联对象的。这相当于把所有对象的关联对象都存在一个全局map里面。而map的的key是这个对象的指针地址（任意两个不同对象的指针地址一定是不同的），而这个map的value又是另外一个AssociationsHashMap，里面保存了关联对象的kv对。
+ 
+        3.3.3 而在对象的销毁逻辑里面见objc-runtime-new.mm:
+ 
+         void *objc_destructInstance(id obj) {
+             if (obj) {
+             Class isa_gen = _object_getClass(obj);
+             class_t *isa = newcls(isa_gen);
+ 
+             // Read all of the flags at once for performance.
+             bool cxx = hasCxxStructors(isa);
+             bool assoc = !UseGC && _class_instancesHaveAssociatedObjects(isa_gen);
+ 
+             // This order is important.
+             if (cxx) object_cxxDestruct(obj);
+             if (assoc) _object_remove_assocations(obj);
+ 
+             if (!UseGC) objc_clear_deallocating(obj);
+ 
+          }
+ 
+             return obj;
+         }
+        runtime的销毁对象函数objc_destructInstance里面会判断这个对象有没有关联对象，如果有，会调用_object_remove_assocations做关联对象的清理工作。
  
  4  分类中添加方方法时原类也有这个方法 这个方法就不调用了，即被category 中的同名方法“替换”掉了
  
@@ -75,7 +117,7 @@
     4.2 category的方法被放到了新方法列表的前面，而原来类的方法被放到了新方法列表的后面，这也就是我们平常所说的category的方法会“覆盖”掉原来类的同名方法，这是因为运行时在查找方法的时候是顺着方法列表的顺序查找的，它只要一找到对应名字的方法，就会停止查找
  
  
-5  怎么调用到原来类中被category覆盖掉的方法？
+ 5  怎么调用到原来类中被category覆盖掉的方法？
     对于这个问题，我们已经知道category其实并不是完全替换掉原来类的同名方法，只是category在方法列表的前面而已，所以我们只要顺着方法列表找到最后一个对应名字的方法，就可以调用原来类的方法
  
  */
