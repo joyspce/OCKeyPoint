505ce2f9fbfe6771367d2ec0b6506226c461c608
jiwuchao <wuchaoji@pptv.com>
2018-08-22 20:42:56 +0800

    customKVO

---
+++ AboutKVO/CustomKVO/CustomKVO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomKVO
+//
+//  Created by JiWuChao on 2018/8/22.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
