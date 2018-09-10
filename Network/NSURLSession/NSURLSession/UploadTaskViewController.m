1d390aa7500ca2dc528383ee5eb90203a2e5e9f2
jiwuchao <wuchaoji@pptv.com>
2018-09-10 22:04:55 +0800

    注释

--- Network/NSURLSession/NSURLSession/UploadTaskViewController.m
+++ Network/NSURLSession/NSURLSession/UploadTaskViewController.m
@@ -130,7 +130,14 @@ didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
 }
 
 /*
- 清求出错的时候调用
+ 任何task完成的时候, 都会调用URLSession:task:didCompleteWithError:方法, error有可能为nil(请求成功), 不为nil(请求失败)
+ 
+ 1 请求失败, 但是该任务是可恢复下载的, 那么error对象的userInfo字典里有一个NSURLSessionDownloadTaskResumeData对应的value, 你应该把这个值传给downloadTaskWithResumeData:方法重新恢复下载
+ 2 请求失败, 但是任务无法恢复下载, 那么应该重新创建一个下载任务并从头开始下载.
+ 3 因为其他原因(如服务器错误等等), 创建并恢复请求.
+ 
+ Note
+ NSURLSession不会收到服务器传来的错误, 代理只会收到客户端出现的错误, 例如无法解析主机名或无法连接上主机等等. 客户端错误定义在URL Loading System Error Codes. 服务端错误通过HTTP状态法进行传输, 详情请看NSHTTPURLResponse和NSURLResponse类.
  */
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(nullable NSError *)error {
     NSLog(@"%s",__func__);
