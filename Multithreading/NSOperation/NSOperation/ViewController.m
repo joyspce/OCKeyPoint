a1eeebb04576222da1e0f45ea625ce97f8d53f65
jiwuchao <wuchaoji@pptv.com>
2018-09-19 21:36:31 +0800

    operation

--- Multithreading/NSOperation/NSOperation/ViewController.m
+++ Multithreading/NSOperation/NSOperation/ViewController.m
@@ -17,7 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 //    [self invocationOperation];
-    [NSThread detachNewThreadSelector:@selector(invocationOperation) toTarget:self withObject:nil];
+//    [NSThread detachNewThreadSelector:@selector(invocationOperation) toTarget:self withObject:nil];
+//    [self blockOperation];
+    [self moreTaskblockOperation];
 }
 
 - (void)invocationOperation {
@@ -26,11 +28,97 @@
 }
 
 - (void)invocationAction {
+    /*
+     在没有使用 NSOperationQueue、在主线程中单独使用使用子类 NSInvocationOperation
+     执行一个操作的情况下，操作是在当前线程执行的，并没有开启新线程。
+     */
     for (int i = 0; i < 2; i++) {
         [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
         NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
     }
 }
+
+
+- (void)blockOperation {
+    /*
+     在没有使用 NSOperationQueue、在主线程中单独使用 NSBlockOperation 执行一个操作的情况下，
+     操作是在当前线程执行的，并没有开启新线程
+     */
+    NSBlockOperation *blcOp = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }]  ;
+    [blcOp start];
+}
+
+/*
+ 总结 ：NSInvocationOperation 和 NSBlockOperation 在哪个线程调用就在哪个线程执行 并不单独生成线程
+ 
+ */
+
+- (void)moreTaskblockOperation {
+    /*
+     在没有使用 NSOperationQueue、在主线程中单独使用 NSBlockOperation 执行一个操作的情况下，
+     操作是在当前线程执行的，并没有开启新线程
+     */
+    NSBlockOperation *blcOp = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }]  ;
+    
+    
+    [blcOp addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    [blcOp addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"3---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    [blcOp addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"4---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    [blcOp addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"5---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    [blcOp addExecutionBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:1]; // 模拟耗时操作
+            NSLog(@"6---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    [blcOp start];
+}
+
+
+/*
+    总结：
+    1 不添加多余的block
+        NSBlockOperation 在哪个线程调用就在哪个线程执行 并不单独生成线程
+    2 添加多余的block
+        使用子类 NSBlockOperation，并调用方法 AddExecutionBlock: 的情况下，blockOperationWithBlock:方法中的操作 和 addExecutionBlock: 中的操作是在不同的线程中异步执行的。而且，这次执行结果中 blockOperationWithBlock:方法中的操作也不是在当前线程（主线程）中执行的。从而印证了blockOperationWithBlock: 中的操作也可能会在其他线程（非当前线程）中执行。
+ 
+    一般情况下，如果一个 NSBlockOperation 对象封装了多个操作。NSBlockOperation 是否开启新线程，取决于操作的个数。如果添加的操作的个数多，就会自动开启新线程。当然开启的线程数是由系统来决定的。
+ */
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
