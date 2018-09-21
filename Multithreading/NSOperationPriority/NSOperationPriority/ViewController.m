83c3df2eb1c69b4e4fbdf1af3f69312d53e17d02
jiwuchao <wuchaoji@pptv.com>
2018-09-21 19:50:16 +0800

    priver

--- Multithreading/NSOperationPriority/NSOperationPriority/ViewController.m
+++ Multithreading/NSOperationPriority/NSOperationPriority/ViewController.m
@@ -16,7 +16,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self operationPriority];
+}
+
+
+- (void)operationPriority {
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"1: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    op1.queuePriority = NSOperationQueuePriorityLow;
+    
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"2: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    op2.queuePriority = NSOperationQueuePriorityNormal;
+    
+    NSBlockOperation *op3 = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"3: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    op3.queuePriority = NSOperationQueuePriorityVeryLow;
+    
+    
+    NSBlockOperation *op4 = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"4: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    op4.queuePriority = NSOperationQueuePriorityHigh;
+    
+    NSBlockOperation *op5 = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"5: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    op5.queuePriority = NSOperationQueuePriorityVeryHigh;
+    
+    
+    
+    [queue addOperation:op1];
+    [queue addOperation:op2];
+    [queue addOperation:op3];
+    [queue addOperation:op4];
+    [queue addOperation:op5];
 }
 
 
