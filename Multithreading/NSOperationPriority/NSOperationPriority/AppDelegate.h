95cf714bc76b6fd2d52b4dbab138d93405f64174
jiwuchao <wuchaoji@pptv.com>
2018-09-21 19:40:54 +0800

    priority

---
+++ Multithreading/NSOperationPriority/NSOperationPriority/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSOperationPriority
+//
+//  Created by JiWuChao on 2018/9/21.
+//  Copyright © 2018年 JiWuChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
