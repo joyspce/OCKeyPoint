dd65666a6730596d64fb111920e3e69868475bca
jiwuchao <wuchaoji@pptv.com>
2018-09-25 15:42:08 +0800

    priority

--- Multithreading/NSOperationPriority/NSOperationPriority/ViewController.m
+++ Multithreading/NSOperationPriority/NSOperationPriority/ViewController.m
@@ -14,6 +14,12 @@
 
 @implementation ViewController
 
+
+/*
+ 1 queuePriority 属性决定了进入准备就绪状态下的操作之间的开始执行顺序。并且，优先级不能取代依赖关系。
+ 2 如果一个队列中既包含高优先级操作，又包含低优先级操作，并且两个操作都已经准备就绪，那么队列先执行高优先级操作。
+ 3 如果，一个队列中既包含了准备就绪状态的操作，又包含了未准备就绪的操作，未准备就绪的操作优先级比准备就绪的操作优先级高。那么，虽然准备就绪的操作优先级低，也会优先执行。优先级不能取代依赖关系。如果要控制操作间的启动顺序，则必须使用依赖关系。
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self operationPriority];
@@ -22,6 +28,15 @@
 
 - (void)operationPriority {
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    
+    NSBlockOperation *op = [NSBlockOperation blockOperationWithBlock:^{
+        for (NSInteger i = 0; i<2; i++) {
+            [NSThread sleepForTimeInterval:1];
+            NSLog(@"0: NSThread name = %@",[NSThread currentThread]);
+        }
+    }];
+    
     NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
         for (NSInteger i = 0; i<2; i++) {
             [NSThread sleepForTimeInterval:1];
@@ -63,8 +78,12 @@
     }];
     op5.queuePriority = NSOperationQueuePriorityVeryHigh;
     
-    
-    
+    [op1 addDependency:op];
+    [op2 addDependency:op];
+    [op3 addDependency:op];
+    [op4 addDependency:op];
+    [op5 addDependency:op];
+    [queue addOperation:op];
     [queue addOperation:op1];
     [queue addOperation:op2];
     [queue addOperation:op3];
