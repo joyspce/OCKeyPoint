0ebdd575416fef8f4dda34f5c8b4ea757808dca2
jiwuchao <wuchaoji@pptv.com>
2018-09-25 17:57:52 +0800

    thread

--- Multithreading/NSThreadDemo/NSThreadDemo/ViewController.m
+++ Multithreading/NSThreadDemo/NSThreadDemo/ViewController.m
@@ -16,8 +16,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+//    [self createThread];
+//    [self createThread2];
+//    [self createThread3];
+    [self threadControl];
 }
+
+- (void)runTest {
+    NSLog(@"%@",[NSThread currentThread]);
+}
+
+- (void)createThread {
+    // 创建线程
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(runTest) object:nil];
+    // 为线程设置名称 有利于调试
+    [thread setName:@"jiwuchao.test.demo"];
+    //启动线程
+    [thread start];
+}
+
+- (void)createThread2 {
+    // 创建线程后自动启动线程 不需要再手动调用 start
+    [NSThread detachNewThreadSelector:@selector(runTest) toTarget:self withObject:nil];
+}
+
+
+- (void)createThread3 {
+    //隐式创建并启动线程
+    [self performSelectorInBackground:@selector(runTest) withObject:nil];
+}
+
+- (void)threadControl {
+    // 创建线程
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(threadControlAc) object:nil];
+    // 为线程设置名称 有利于调试
+    [thread setName:@"jiwuchao.test.demo"];
+    //启动线程
+    [thread start];
+    
+    
+    
+}
+
+- (void)threadControlAc {
+    for (NSInteger i = 0; i <= 10 ; i ++) {
+        NSLog(@"i = %ld  current thread %@",i,[NSThread currentThread]);
+        [NSThread sleepForTimeInterval:1];
+        if (i == 5) {
+            NSLog(@"线程退出");
+            [NSThread exit];
+            NSLog(@"不在执行这一步");
+        }
+    }
+}
+
+
+
 
 
 @end
