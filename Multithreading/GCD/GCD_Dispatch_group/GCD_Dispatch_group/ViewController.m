4a40b801fa320956a31a9b555a71cd7e96f5cf0f
jiwuchao <wuchaoji@pptv.com>
2018-09-26 22:04:25 +0800

    group

--- Multithreading/GCD/GCD_Dispatch_group/GCD_Dispatch_group/ViewController.m
+++ Multithreading/GCD/GCD_Dispatch_group/GCD_Dispatch_group/ViewController.m
@@ -16,7 +16,132 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    [self dispathcGroupNotify];
+//    [self dispathcGroupWait];
+    [self dispathcGroupLeave];
+}
+
+- (void)dispathcGroupNotify {
+    NSLog(@"currentThread---%@",[NSThread currentThread]);
+    NSLog(@"group---begin");
+    dispatch_group_t group = dispatch_group_create();
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.jiwuchao.www", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue1 = dispatch_queue_create("com.jiwuchao.www1", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue2 = dispatch_queue_create("com.jiwuchao.www2", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_group_async(group, queue, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"1---%@",[NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_async(group, queue1, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"2---%@",[NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_async(group, queue2, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"3---%@",[NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        // 等前面的异步任务1、任务2都执行完毕后，回到主线程执行下边任务
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"4---%@",[NSThread currentThread]);
+        }
+        NSLog(@"group---end");
+    });
+}
+
+- (void)dispathcGroupWait {
+    NSLog(@"currentThread---%@",[NSThread currentThread]);
+    NSLog(@"group---begin");
+    dispatch_group_t group = dispatch_group_create();
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.jiwuchao.www", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue1 = dispatch_queue_create("com.jiwuchao.www1", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue2 = dispatch_queue_create("com.jiwuchao.www2", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_group_async(group, queue, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"1---%@",[NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_async(group, queue1, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"2---%@",[NSThread currentThread]);
+        }
+    });
+    
+    dispatch_group_async(group, queue2, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"3---%@",[NSThread currentThread]);
+        }
+    });
+    // 阻塞 20 秒
+    dispatch_group_wait(group, dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20 * NSEC_PER_SEC)));
+    NSLog(@"group---end");
+    
+
+}
+
+- (void)dispathcGroupLeave {
+    NSLog(@"currentThread---%@",[NSThread currentThread]);
+    NSLog(@"group---begin");
+    dispatch_group_t group = dispatch_group_create();
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.jiwuchao.www", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue1 = dispatch_queue_create("com.jiwuchao.www1", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t queue2 = dispatch_queue_create("com.jiwuchao.www2", DISPATCH_QUEUE_CONCURRENT);
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"1---%@",[NSThread currentThread]);
+        }
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue1, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"2---%@",[NSThread currentThread]);
+        }
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_enter(group);
+    dispatch_async(queue2, ^{
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];
+            NSLog(@"3---%@",[NSThread currentThread]);
+        }
+        dispatch_group_leave(group);
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        // 等前面的异步操作都执行完毕后，回到主线程.
+        for (int i = 0; i < 2; ++i) {
+            [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
+            NSLog(@"4---%@",[NSThread currentThread]);      // 打印当前线程
+        }
+        NSLog(@"group---end");
+    });
+
 }
 
 
